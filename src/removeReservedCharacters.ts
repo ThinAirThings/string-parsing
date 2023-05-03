cba381abe30b97bcb2ab24855e3cbb33f0632544
irishcarbomb777 <danielclannan@gmail.com>
2023-05-03 08:54:56 -0400

    Initial commit

---
+++ src/removeReservedCharacters.ts
@@ -0,0 +1,12 @@
+
+export function removeReservedCharacters(str: string): string {
+  // List of reserved characters to remove
+  const reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
+
+  // Replace each reserved character with an empty string
+  for (const char of reservedChars) {
+    str = str.replace(new RegExp(`\\${char}`, 'g'), '');
+  }
+
+  return str;
+}
