f9d947bca24e31672fe253bd912ac1f6498afd65
irishcarbomb777 <danielclannan@gmail.com>
2023-05-03 09:42:19 -0400

    mod

--- dist/removeReservedCharacters.js
+++ dist/removeReservedCharacters.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeReservedCharacters = void 0;
 function removeReservedCharacters(str) {
     // List of reserved characters to remove
-    const reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
+    const reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', ' '];
     // Replace each reserved character with an empty string
     for (const char of reservedChars) {
         str = str.replace(new RegExp(`\\${char}`, 'g'), '');
--- src/removeReservedCharacters.ts
+++ src/removeReservedCharacters.ts
@@ -1,7 +1,7 @@
 
 export function removeReservedCharacters(str: string): string {
   // List of reserved characters to remove
-  const reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
+  const reservedChars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', ' '];
 
   // Replace each reserved character with an empty string
   for (const char of reservedChars) {
